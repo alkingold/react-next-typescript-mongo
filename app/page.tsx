9490b1aeb9b9a2f2fb7536e163ed5b24037acad3
Alexandra Bogdanova <alexandra.kaichev@gmail.com>
2025-02-06 18:50:02 +0100

    09 Implement handleCreateTask function

--- app/page.tsx
+++ app/page.tsx
@@ -7,9 +7,28 @@ import AddTask from "@/components/AddTask"
 
 export default function Home() {
   const [task, setTask] = useState('')
+  const [isLoading, setIsLoading] = useState(true)
 
   const handleCreateTask = async () => {
+    setIsLoading(true)
 
+    try {
+      const response = await fetch("/api/task/new", {
+        method: 'POST',
+        body: JSON.stringify({
+          task
+        })
+      })
+      if (response.ok) {
+        setTask('')
+      } else {
+        console.log('error')
+      }
+    } catch (error) {
+      console.error(error)
+    }
+
+    setIsLoading(false)
   }
 
   return (
